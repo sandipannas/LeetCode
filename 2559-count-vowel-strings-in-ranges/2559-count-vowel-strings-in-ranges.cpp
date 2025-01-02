3954b253098a3029c9a2c6e4300bc7d37d2f9e96
sandipannas <132623998+sandipannas@users.noreply.github.com>
2025-01-02 07:53:09 +0530

    Time: 222 ms (5.16%), Space: 73.8 MB (32.74%) - LeetHub

---
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        int index=0;
+        int sum=0;
+        vector<int> og(words.size());
+        for(auto s:words)
+        {   int n=s.size()-1;
+            if((s[0]=='a'||s[0]=='e'||s[0]=='i'||s[0]=='o'||s[0]=='u') &&
+               (s[n]=='a'||s[n]=='e'||s[n]=='i'||s[n]=='o'||s[n]=='u'))
+               {
+                sum++;
+                words[index]="1";
+               }
+               else
+               {
+                words[index]="0";
+               }
+               og[index]=sum;
+
+               index++;
+        }
+        for(auto i:words)
+        {
+            cout<<i<<" ";
+        }
+        index=0;
+        vector<int> gg(queries.size(),0);
+        for(auto i:queries)
+        {
+           if(i[0]==i[1])
+           {
+            gg[index]=stoi(words[i[0]]);
+           }
+           else
+           {
+            gg[index]=og[i[1]]-og[i[0]]+stoi(words[i[0]]);
+           }
+           index++; 
+        }
+        return gg;
+    }
+};
