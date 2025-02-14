02d8dd08374ec080632a80cf149035b65f119cc7
sandipannas <132623998+sandipannas@users.noreply.github.com>
2025-02-14 12:17:52 +0530

    Time: 182 ms (31.53%), Space: 80 MB (5.54%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,58 @@
+class ProductOfNumbers {
+public:
+    vector<unsigned long long> gg;
+    vector<int> og;
+    long long count=0;
+    ProductOfNumbers() {
+        
+    }
+    
+    void add(int num) {
+        if(num==0){
+        count++; num=1;
+        if(gg.size()==0){
+        gg.push_back(num);
+        og.push_back(count);
+        }
+        
+        else{
+        gg.push_back(num);
+        og.push_back(count);
+        }
+        return;
+        }
+
+        if(gg.size()==0){
+        gg.push_back(num);
+        og.push_back(count);
+        }
+        
+        else{
+        gg.push_back(gg.back()*num);
+        og.push_back(count);
+        }
+        cout<<num<<" "<<gg.back()<<endl;
+    }
+    
+    int getProduct(int k) {
+        
+        if(gg.size()==0) { return 0; }
+
+        if(k==gg.size()){
+            if(count>0){return 0;}
+            return gg.back();
+            }
+
+        if(og.back()-og[og.size()-k-1]>0) { return 0; }
+        if(gg[gg.size()-k-1]==0){return gg.back();}
+        
+        return gg.back()/gg[gg.size()-k-1];
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
