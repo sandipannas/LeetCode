b5e5d589aa560eb90c19d851bcb4991c101addea
sandipannas <132623998+sandipannas@users.noreply.github.com>
2025-02-15 23:49:23 +0530

    Time: 103 ms (86.67%), Space: 182.1 MB (23.33%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    double separateSquares(vector<vector<int>>& squares) {
+        double ta=0;
+        double hi=0; 
+        double low=0; 
+        for(auto& a:squares)
+            {
+                ta+=pow(a[2],2);
+                hi=max(hi,(double)a[1]+a[2]);
+            }
+        double eps=1e-6;
+        double ff=-1;
+        //cout<<ta/2<<endl;
+        while(hi-low>eps)
+        {   double mid=(hi+low)/2;
+            long double tem=0;
+            
+            for(auto& a:squares)
+            { if(mid>a[1] && mid>=a[1]+a[2]){ tem+=(double)a[2]*(double)a[2];}
+              else if(mid>a[1] && mid<a[1]+a[2]){tem+=(mid-a[1])*a[2];}
+            }
+            //cout<<low<<"    "<<mid<<"    "<<hi<<"    "<<tem<<endl;
+
+            if(ta/2>tem){ low=mid;}
+            else{
+                hi=mid;
+            }
+            ff=mid;
+        }
+        return ff;
+        
+    }
+};
