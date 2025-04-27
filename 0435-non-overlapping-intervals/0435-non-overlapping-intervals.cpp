57ef0ddaf1d9b38c3ad6cd5b2d8ce7b9047e40ef
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-04-28 03:46:50 +0530

    Time: 35 ms (95.91%), Space: 93.9 MB (67.14%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,9 +4,9 @@ public:
     sort(intervals.begin(),intervals.end(),[](const auto& a,const auto& b){
         return a[1]<b[1];
     });
-    int count=1;
-    int last=intervals[0][1];
-    for(int i=1;i<intervals.size();i++){
+    int count=0;
+    int last=INT_MIN;
+    for(int i=0;i<intervals.size();i++){
         if(intervals[i][0]>=last){
             last=intervals[i][1];
             count++;
