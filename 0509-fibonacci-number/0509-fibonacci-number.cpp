0259e46890d2c9c44bcc6e9a87390311e0b62d33
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-08-02 01:10:37 +0530

    Time: 3 ms (66.45%), Space: 7.8 MB (36.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int fib(int n) {
        if(n<=1){return n;}
-       vector<int> dp(n+1);dp[0]=0;dp[1]=1;
+       int dp1=0,dp2=1,ans=-1;
        for(int i=2;i<=n;i++)
        {
-        dp[i]=dp[i-1]+dp[i-2];
+         ans=dp1+dp2;
+         dp1=dp2;dp2=ans;
        }
-       return dp[n];
+       return ans;
     }
 };
