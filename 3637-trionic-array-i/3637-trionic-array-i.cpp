281ca46b850b2953cee63ec4d7961768eac87a31
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-08-05 22:26:33 +0530

    Time: 3 ms (19.16%), Space: 27.8 MB (38.95%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=1;i<nums.size()-1;i++){
           if(nums[i]>nums[i-1] && nums[i]>nums[i+1]) {count+="a";} 
           if(nums[i]<nums[i-1] && nums[i]<nums[i+1]) { count+="b";}
-          if(nums[i]==nums[i-1]){return false;}
+          if(nums[i]==nums[i-1] || nums[i]==nums[i+1]){return false;}
         }
         if(count=="ab"){return true;}
         return false;
