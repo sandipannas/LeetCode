c0145c13e663f8e98f4b2beb748aec34bf276391
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-09-02 19:11:30 +0530

    Time: 787 ms (5.19%), Space: 168.3 MB (5.19%) - LeetHub

---
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        
+        sort(points.begin(),points.end(),[](const vector<int>& a,const vector<int>& b) {
+        if (a[0] == b[0]) 
+            return a[1] > b[1];  
+        return a[0] < b[0];      
+        });
+        int count=0;        
+        for(int i=0;i<points.size();i++){
+            for(int j=i+1;j<points.size();j++){
+                vector<int> a = points[i];
+                vector<int> b = points[j];
+                
+                if(a[0]!=b[0] && a[1]<b[1]){
+                    continue;
+                }
+                
+                int flag=1;
+                for(int k=0;k<points.size();k++){
+                    if(k==i || k==j){ continue; } 
+                    vector<int> p = points[k];
+                    if((p[0]>=a[0] && p[0]<=b[0]) && (p[1]<=a[1] && p[1]>=b[1])){
+                        flag=0;
+                        break;
+                    }
+                }
+                
+                if(flag){
+                cout<<a[0]<<","<<a[1]<<" and "<<b[0]<<","<<b[1]<<endl;
+                count++;
+                }
+
+            }
+        }
+        return count;
+    }
+};
