25ede5bf62ca432a79dca89bdc63554799f75b4a
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-09-10 03:02:37 +0530

    Time: 0 ms (100%), Space: 9.4 MB (69.82%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        const int mod = 1e9 + 7;
+        vector<long long> dp(n + 1, 0);
+        dp[1] = 1;
+        long long total = 0;
+        for (int i = 2; i <= n; i++){
+            if (i - delay > 0){
+                total = (total + dp[i - delay] + mod) % mod;
+            }
+            if (i - forget > 0){
+                total = (total - dp[i - forget] + mod) % mod;
+            }
+            dp[i] = total;
+        }
+        long long res = 0;
+        for (int i = n - forget + 1; i <= n; i++){
+            res = (res + dp[i]) % mod;
+        }
+        return (int)res;
+    }
+};
