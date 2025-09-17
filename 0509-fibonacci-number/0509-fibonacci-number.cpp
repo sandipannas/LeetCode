00b40e342acdd7d21e2aa27d657739fa4d68309a
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-09-17 12:05:45 +0530

    Time: 3 ms (66.45%), Space: 8.4 MB (11.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,14 @@
+//memoization (recursion-top-down)
 class Solution {
 public:
+    int fibo(int n, vector<int>& dp ){
+        if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    }
     int fib(int n) {
-       if(n<=1){return n;}
-       int dp1=0,dp2=1,ans=-1;
-       for(int i=2;i<=n;i++)
-       {
-         ans=dp1+dp2;
-         dp1=dp2;dp2=ans;
-       }
-       return ans;
+       vector<int> dp(n+1,-1);
+       return fibo(n,dp);
+
     }
 };
