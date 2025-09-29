f1dfa26040f54e5f9f1bbde4d6509207dcb755ae
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-09-30 04:52:10 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.76%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==1){ return nums[0];}
-        if(nums.size()==2){ return max(nums[0],nums[1]);}
-        int n=nums.size();
-        nums.push_back(nums[0]);
-        vector<int> dp(n+1,-1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[1],nums[0]);
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        if(nums.size()==1)return nums[0];
+        if(nums.size()==2)return max(nums[0],nums[1]);
+
+        int dp1=nums[0];
+        int dp2=max(nums[0],nums[1]);
+
+        for(int i=2;i<nums.size()-1;i++){
+            int tem = max(dp2,dp1+nums[i]);
+            dp1=dp2;
+            dp2=tem;
         }
-        return dp[n]-nums[0];
+
+        int dpp1=0;
+        int dpp2=nums[1];
+
+        for(int i=2;i<nums.size();i++){
+            int tem = max(dpp2,dpp1+nums[i]);
+            dpp1=dpp2;
+            dpp2=tem;
+        }
+
+        return max(dpp2,dp2);
+        
     }
+    
 };
