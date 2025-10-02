042b43690ad11368a6ed162b7fad097e7918e48c
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-10-02 13:10:36 +0530

    Time: 0 ms (100%), Space: 12.6 MB (80.35%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -6,14 +6,10 @@ public:
         vector<int> dpDown(triangle[m-1].size()+1);
         
         for(int i=m-1;i>=0;i--){
-
-            vector<int> store(triangle[i].size());
-
-            for(int j=triangle[i].size()-1;j>=0;j--){
+            for(int j=0;j<triangle[i].size();j++){
                 int tem = triangle[i][j]+min(dpDown[j],dpDown[j+1]);
-                store[j]=tem;
+                dpDown[j]=tem;
             }
-            dpDown=store;
         }
         return dpDown[0];
     }
