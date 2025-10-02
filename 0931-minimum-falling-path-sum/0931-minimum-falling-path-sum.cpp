8402375a98cbbe8b2f19b65bbaf5a976a15b3890
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-10-02 15:32:24 +0530

    Time: 1241 ms (5.11%), Space: 15 MB (39.72%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> dpDown(n,0);
+
+        for(int i=m-1;i>=0;i--){
+            vector<int> store(n,INT_MAX);
+            for(int j=n-1;j>=0;j--){
+
+                cout<<"the option for "<<matrix[i][j]<<"are "<<"\n";
+                
+                int left = j-1<0 || i+1>=m ?INT_MAX:dpDown[j-1];
+                int down = i+1>=m ?INT_MAX:dpDown[j];
+                int right = j+1>=n || i+1>=m ?INT_MAX:dpDown[j+1];
+                
+                cout<<"left "<<left<<"\n";
+                cout<<"down "<<down<<"\n";
+                cout<<"right "<<right<<"\n";
+
+                int tem =matrix[i][j]+((left==down && down==right && left==right && right==INT_MAX)?0:min(left,min(down,right)));
+
+                cout<<"so the total is "<<tem<<"\n";
+
+                store[j]=tem;
+            }
+            dpDown=store;
+        }
+        int miin=INT_MAX;
+        for(int i:dpDown){
+            miin=min(i,miin);
+        }
+        return miin;
+    }
+};
