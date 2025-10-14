2e8587de0f0309b09581f3012fd9e2e538e3d83e
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-10-14 09:22:04 +0530

    Time: 12 ms (68.58%), Space: 40.8 MB (23.45%) - LeetHub

---
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        if(k==1)return true;
+        int curr=0;
+        int pre=INT_MIN;
+        unordered_set<int> set;
+
+        for(int i=0;i<nums.size();i++){
+           if(nums[i]>pre){
+            curr++;
+           }
+           else{
+            curr=1;
+           }
+           if(curr>=k){
+             set.insert(i);
+           }
+           pre=nums[i];
+        }
+        for(int i:set){
+            if(set.count(i-k)){
+                return true;
+            }
+            cout<<i<<endl;
+        }
+        return false;
+    }
+};
