c4956b507d7f494c3cac893277b9856925709d85
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-12-12 03:52:10 +0530

    Time: 11 ms (33.37%), Space: 7.8 MB (54.71%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -4,9 +4,10 @@ public:
       int count=0;
       for(int a=1;a<=n;a++){
         for(int b=1;b<=n;b++){
-            for(int c=1;c<=n;c++){
-                if(pow(a,2)+pow(b,2)==pow(c,2)){ count++; }
-            }
+            float c=pow(a,2)+pow(b,2);
+            c=sqrt(c);
+            if(c<=(float)n && fmod(c,1.0)==0){ count++; }
+            //cout<<a<<" "<<b<<" "<<c<<" "<<fmod(c,1.0)<<endl;
         }
       }
       return count;   
