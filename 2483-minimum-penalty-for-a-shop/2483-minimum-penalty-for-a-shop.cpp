30f4a6f151c836fbba1c03198546a4d899561e32
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-12-26 21:35:19 +0530

    Time: 4 ms (81.86%), Space: 13.4 MB (81.2%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-       vector<int> prefix;
-       int count=0;
-       for(char i:customers){
-        if(i=='Y'){
-          count++;
-        }
-        prefix.push_back(count);
-       }
-       int limit=INT_MAX;
-       int answer=-1;
+       int penalty=0,minPenalty=0;
+       int answer=0;
 
-       for(int j=0;j<=customers.size();j++){
-         //penalty=no customer before closing + yes customer after closing
-         int yesBefore=(j==0)?0:prefix[j-1];
-         int yesAfter=prefix[customers.size()-1]-yesBefore;
-         int noBefore=j-yesBefore;
-         
-         int penalty=noBefore+yesAfter;
-         if(limit>penalty){
-            limit=penalty;
+       for(int j=1;j<=customers.size();j++){
+          int toAdd = customers[j-1]=='N'?1:-1;
+          penalty+=toAdd;
+
+          if(minPenalty>penalty){
+            minPenalty=penalty;
             answer=j;
-         }
-
-         //cout<<yesBefore<<"  "<<yesAfter<<endl;
+          }
        }
        return answer;
     }
