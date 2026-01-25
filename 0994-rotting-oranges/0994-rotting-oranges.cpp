02857d827ca803233018e4ae02e6af26261e07c8
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-01-26 01:29:09 +0530

    Time: 3 ms (28.84%), Space: 17.2 MB (19.7%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,51 +1,77 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        
-        int m=grid.size(),n=grid[0].size();
-        unordered_map<int,vector<int>> graph;
-        unordered_map<int,int> lowest_value;
-        vector<int> rot;
-        int node=0;
+        int m=grid.size();
+        int n=grid[0].size();
 
+        queue<pair<int,int>> line;
+        vector<vector<int>> visited(m,vector(n,0));
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-              if(grid[i][j]==0){ node++; continue; }
-              if(grid[i][j]==2){ rot.push_back(node); }
-              lowest_value[node]=INT_MAX;
-            
-              if(i-1>=0 && grid[i-1][j]!=0) { graph[node].push_back(node-n);} 
-              if(i+1<m && grid[i+1][j]!=0) { graph[node].push_back(node+n);} 
-              if(j-1>=0 && grid[i][j-1]!=0) { graph[node].push_back(node-1);} 
-              if(j+1<n && grid[i][j+1]!=0) { graph[node].push_back(node+1);} 
-              node++;
-            }
-        }
-        if(lowest_value.size()==0){ return 0;}
-        
-        queue<int> kwi;
-        unordered_set<int> visited;
-        for(int i:rot){
-            kwi.push(i);
-            visited.insert(i);
-            lowest_value[i]=0;
-        }
-        while(!kwi.empty()){
-                int gg=kwi.front();
-                kwi.pop();
-                for(int ii:graph[gg]){
-                    if(visited.count(ii)){ continue; }
-                    visited.insert(ii);
-                    kwi.push(ii);
-                    lowest_value[ii]=min(lowest_value[ii],lowest_value[gg]+1);
+                if(grid[i][j]==2){
+                    pair<int,int> tem={i,j};
+                    line.push(tem);
+                    visited[i][j]=1;
                 }
             }
-        int ans=0;
-        for(auto& i:lowest_value){
-            ans=max(i.second,ans);
         }
-        if(ans==INT_MAX){return -1;}
-        return ans;
+
+        line.push({-1,-1});
+
+        int count=0;
+        
+        while(line.size()>1){
+            if(line.front().first==-1){
+              count++;
+              line.pop();
+              line.push({-1,-1});
+            }
+            else{
+                 int row=line.front().first;
+                 int col=line.front().second;
+                 line.pop();
+                 
+        
+
+                 //up
+                 if((row-1>=0 && grid[row-1][col]==1) && visited[row-1][col]==0){
+                    visited[row-1][col]=1;
+                    grid[row-1][col]=2;
+                    line.push({row-1,col});
+                 }
+                 //right
+                if(col+1<n && grid[row][col+1]==1 && visited[row][col+1]==0){
+                    visited[row][col+1]=1;
+                    grid[row][col+1]=2;
+                    line.push({row,col+1});
+                 }
+                 //left
+                if(col-1>=0 && grid[row][col-1]==1 && visited[row][col-1]==0){
+                    visited[row][col-1]=1;
+                    grid[row][col-1]=2;
+                    line.push({row,col-1});
+                 }
+                 //down
+                if(row+1<m && grid[row+1][col]==1 && visited[row+1][col]==0){
+                    visited[row+1][col]=1;
+                    grid[row+1][col]=2;
+                    line.push({row+1,col});
+                 }
+            }
+
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+
+        return count;
+
+
     }
 };
