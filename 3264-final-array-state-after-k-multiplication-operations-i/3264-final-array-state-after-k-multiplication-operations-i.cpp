a9e7bf82ba542057bfc8f2f1845c421fa2284cd1
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-03-10 00:33:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
 
-        for(int i;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
            pq.push({nums[i],i});
         }
 
