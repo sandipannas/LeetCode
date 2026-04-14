637c0c12c858836c80eab37d2f89a12ed7981bf5
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-04-15 01:26:37 +0530

    Time: 2 ms (20%), Space: 8.9 MB (68.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    //space optimized solution
     int uniquePaths(int m, int n) {
-       vector<int> dpRight(m,0);
-       vector<int> dpDown(n,0);
+        vector<int> right(m,0);
+        vector<int> down(n,0);
 
-       dpRight[m-1]=1;
+        right[m-1]=1;
 
-       for(int i=m-1;i>=0;i--){
-        for(int j=n-1;j>=0;j--){
-            int down=dpDown[j];
-            int right=dpRight[i];
-            int tem=down+right;
-            dpDown[j]=tem;
-            dpRight[i]=tem;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int ways=right[i]+down[j];
+                right[i]=ways; down[j]=ways;
+            }
         }
-       }
-       return dpDown[0];
+
+        return right[0];
     }
 };
