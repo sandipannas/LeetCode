40ebeda1b1dac312631c66c870410f9725340bdc
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-05-21 02:26:51 +0530

    Time: 0 ms (100%), Space: 17 MB (38.91%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,72 +5,63 @@ public:
         int n=grid[0].size();
 
         queue<pair<int,int>> line;
-        vector<vector<int>> visited(m,vector(n,0));
+        vector<vector<int>> visited(m,vector<int>(n));
 
+        //inserting the rotten oranges
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    pair<int,int> tem={i,j};
-                    line.push(tem);
+                    line.push({i,j});
                     visited[i][j]=1;
                 }
             }
         }
 
         line.push({-1,-1});
-
         int count=0;
-        
-        while(line.size()>1){
-            if(line.front().first==-1){
-              count++;
-              line.pop();
-              line.push({-1,-1});
-            }
-            else{
-                 int row=line.front().first;
-                 int col=line.front().second;
-                 line.pop();
-                 
-        
 
-                 //up
-                 if((row-1>=0 && grid[row-1][col]==1) && visited[row-1][col]==0){
-                    visited[row-1][col]=1;
-                    grid[row-1][col]=2;
-                    line.push({row-1,col});
-                 }
-                 //right
-                if(col+1<n && grid[row][col+1]==1 && visited[row][col+1]==0){
-                    visited[row][col+1]=1;
-                    grid[row][col+1]=2;
-                    line.push({row,col+1});
-                 }
-                 //left
-                if(col-1>=0 && grid[row][col-1]==1 && visited[row][col-1]==0){
-                    visited[row][col-1]=1;
-                    grid[row][col-1]=2;
-                    line.push({row,col-1});
-                 }
-                 //down
-                if(row+1<m && grid[row+1][col]==1 && visited[row+1][col]==0){
-                    visited[row+1][col]=1;
-                    grid[row+1][col]=2;
-                    line.push({row+1,col});
-                 }
+        while(line.size()!=1){
+            pair<int,int> current=line.front();
+            line.pop();
+
+            int i=current.first;
+            int j=current.second;
+
+            if(i==-1){
+                count++; 
+                line.push({-1,-1}); 
+                continue;
             }
 
+            //up
+            if(i-1>=0 && !visited[i-1][j] && grid[i-1][j]!=0){
+                visited[i-1][j]=1;
+                line.push({i-1,j});
+            }
+            //down
+            if(i+1<m && !visited[i+1][j] && grid[i+1][j]!=0){
+                visited[i+1][j]=1;
+                line.push({i+1,j});
+            }
+            //left
+            if(j-1>=0 && !visited[i][j-1] && grid[i][j-1]!=0){
+                visited[i][j-1]=1;
+                line.push({i,j-1});
+            }
+            //right
+            if(j+1<n && !visited[i][j+1] && grid[i][j+1]!=0){
+                visited[i][j+1]=1;
+                line.push({i,j+1});
+            }
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
+                if(visited[i][j]==0 && grid[i][j]==1){ return -1;}
             }
         }
-
-        return count;
+       
+       return count;
 
 
     }
