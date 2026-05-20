b120cac0132c9c0862c79cc1ef0368fb1352c4e8
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-05-21 04:13:33 +0530

    Time: 4 ms (6.06%), Space: 18.2 MB (56.67%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
-    void colorr(vector<vector<int>>& image, int sr, int sc, int color, int original){
-        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || image[sr][sc]!=original || image[sr][sc]==color){ return;}
-
-        image[sr][sc]=color;
-        //up
-        colorr(image,sr-1,sc,color,original);
-        //right
-        colorr(image,sr,sc+1,color,original);
-        //left
-        colorr(image,sr,sc-1,color,original);
-        //down
-        colorr(image,sr+1,sc,color,original);
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int original = image[sr][sc];
 
-        if(color==original){ return image; }
-        colorr(image,sr,sc,color,original);
+        if(image[sr][sc]==color){ return image; }
 
+        int m=image.size();
+        int n=image[0].size();
+        queue<pair<int,int>> line;
+
+        int original=image[sr][sc];
+        
+        image[sr][sc]=color;
+        line.push({sr,sc});
+
+        while(line.size()!=0){
+            //cout<<line.size()<<endl;
+            int i=line.front().first;
+            int j=line.front().second;
+            line.pop();
+
+            //up
+            if(i-1>=0 && image[i-1][j]==original){ 
+                //cout<<"pushing "<<i-1<<" "<<j<<endl;
+                image[i-1][j]=color; 
+                line.push({i-1,j});
+            }
+            //down
+            if(i+1<m && image[i+1][j]==original){ 
+                //cout<<"pushing "<<i+1<<" "<<j<<endl;
+                image[i+1][j]=color; 
+                line.push({i+1,j});
+            }
+            //left
+            if(j-1>=0 && image[i][j-1]==original){ 
+                //cout<<"pushing "<<i<<" "<<j-1<<endl;
+                image[i][j-1]=color; 
+                line.push({i,j-1});
+            }
+            //right
+            if(j+1<n && image[i][j+1]==original){ 
+                //cout<<"pushing "<<i<<" "<<j+1<<endl;
+                image[i][j+1]=color; 
+                line.push({i,j+1});
+            }
+        }
         return image;
+
     }
 };
