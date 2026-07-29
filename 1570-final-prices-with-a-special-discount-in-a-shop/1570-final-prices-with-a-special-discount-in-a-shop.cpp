1732346c3909eb208bd5e0d689901c29a076d394
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-07-30 01:28:59 +0530

    1570-FINAL-PRICES-WITH-A-SPECIAL-DISCOUNT-IN-A-SHOP Stats: Time: 0 ms (100%), Space: 14.3 MB (30.27%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,16 +3,14 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         stack<int> stk;
 
-        int i=0;
-        while(i<prices.size()){
-            if(!stk.empty() && prices[stk.top()]>=prices[i]){
+        for(int i=0;i<prices.size();i++){
+            while(!stk.empty() && prices[stk.top()]>=prices[i]){
 
                 prices[stk.top()]-=prices[i];
-                stk.pop(); continue;
-                
+                stk.pop();
+
             }
             stk.push(i);
-            i++;
         }
 
         return prices;
