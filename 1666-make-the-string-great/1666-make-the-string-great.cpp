e3e6fb72b30b78e081c1f3969f28812e25b878c1
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-07-30 02:12:22 +0530

    1666-MAKE-THE-STRING-GREAT Stats: Time: 0 ms (100%), Space: 8.9 MB (26.13%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.cpp
+++ 1666-make-the-string-great/1666-make-the-string-great.cpp
@@ -3,19 +3,16 @@ public:
     string makeGood(string s) {
         stack<char> stk;
         for(int i=0;i<s.size();i++){
-            while(!stk.empty() && abs((int)stk.top()-(int)s[i])==32 ){
-               
+            if(!stk.empty() && abs((int)stk.top()-(int)s[i])==32 )
                 stk.pop();
-                i++;
-            }
-            if(i<s.size()) stk.push(s[i]);
+            else{ stk.push(s[i]); }
         }
         s="";
         while(!stk.empty()){
             s.push_back(stk.top()); stk.pop();
         }
-
-        if(!s.empty()) reverse(s.begin(),s.end());
+        
+        reverse(s.begin(),s.end());
 
         return s;
 
