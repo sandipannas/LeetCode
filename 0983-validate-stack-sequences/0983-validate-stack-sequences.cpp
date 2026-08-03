053f98ec9442abe0b6679ff84c0bf5928104b7bd
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-03 14:33:22 +0530

    0983-VALIDATE-STACK-SEQUENCES Stats: Time: 0 ms (100%), Space: 19.2 MB (13.59%) - LeetHub

--- 0983-validate-stack-sequences/0983-validate-stack-sequences.cpp
+++ 0983-validate-stack-sequences/0983-validate-stack-sequences.cpp
@@ -5,13 +5,18 @@ public:
         int pt=0;
 
         for(int i=0;i<pushed.size();i++){
-            if(!stk.empty() && popped[pt]==stk.top()){ stk.pop(); cout<<"pop "<<popped[pt]<<endl; pt++; i--;}
-            else{ stk.push(pushed[i]); cout<<"pushed "<<pushed[i]<<endl;}
+            if(!stk.empty() && popped[pt]==stk.top()){ 
+                stk.pop();  
+                pt++; 
+                i--;
+            }
+            else{ 
+                stk.push(pushed[i]); 
+            }
         }
 
         while(pt<popped.size()){
             if(stk.empty() || stk.top()!=popped[pt]) return false;
-            cout<<"poped "<<stk.top()<<endl;
             stk.pop(); pt++;
         }
 
