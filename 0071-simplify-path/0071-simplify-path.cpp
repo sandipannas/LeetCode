5495037d4cdbdec6b899e2a1285db34ae0b8faaa
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-27 21:43:27 +0530

    Time: 0 ms (100%), Space: 11.4 MB (51.28%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> canonical;
+
+        path+="/";
+        string tem="";
+        for(char c:path){
+           if(c=='/'){
+                if(tem.empty()){ continue; }
+                else if(tem=="."){ }
+                else if(tem==".."){ 
+                    if(!canonical.empty()) canonical.pop_back(); 
+                }
+                else{ canonical.push_back(tem); }
+                tem="";
+           }
+           else{
+            tem+=c;
+           }
+        }
+
+        if(!tem.empty() && tem!="." && tem!=".."){ 
+            canonical.push_back(tem); 
+        }
+
+        if(canonical.empty()) return "/";
+
+        string answer="";
+        for(string s:canonical){
+            answer+=("/"+s);
+        }
+
+        return answer;
+    }
+};
