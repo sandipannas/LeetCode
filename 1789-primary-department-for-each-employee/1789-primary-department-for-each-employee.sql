83913e8443e4861b11a388a3d7d6ae783000bf6a
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-30 15:31:12 +0530

    Time: 270 ms (22.53%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,14 @@
+select
+    employee_id,
+    department_id
+from employee
+where primary_flag='Y'
+
+union
+
+select
+    employee_id,
+    min(department_id) as department_id
+from employee
+group by employee_id
+having count(*)=1
