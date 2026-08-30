d1c9181a8770dd47465fca15c44fa1b3ceb3c039
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-31 01:26:19 +0530

    Time: 227 ms (82.39%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,12 +1,17 @@
-select 
+with remp as
+(select 
     d.name as Department,
     e.name as Employee,
-    e.salary as Salary
+    e.salary as Salary,
+    dense_rank() over(partition by d.id order by e.salary desc) as rank
 from employee as e
 inner join department as d
 on e.departmentId=d.id
-where (e.salary,e.departmentId) in ( 
-    select max(salary), departmentId
-    from employee
-    group by departmentId
-    )
+)
+
+select 
+    Department,
+    Employee,
+    Salary
+from remp
+where rank=1
