083d9927bca97fe3749e6f3e49f25adeb4e1eb1d
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-31 00:26:33 +0530

    Time: 182 ms (98.69%), Space: 0B (100%) - LeetHub

---
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -0,0 +1,21 @@
+select
+    sample_id,
+    dna_sequence,
+    species,
+    case
+        when regexp_like(dna_sequence,'^ATG') then 1
+        else 0
+    end as has_start,
+    case
+        when regexp_like(dna_sequence,'TAA$') or regexp_like(dna_sequence,'TAG$') or regexp_like(dna_sequence,'TGA$') then 1
+        else 0
+    end as has_stop,
+    case
+        when regexp_like(dna_sequence,'ATAT') then 1
+        else 0
+    end as has_atat,
+    case
+        when regexp_like(dna_sequence,'GGG') then 1
+        else 0
+    end as has_ggg
+from samples    
