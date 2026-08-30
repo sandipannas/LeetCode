e1f44ceb300c16dadc68bfde8af340b21cdc0580
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-30 14:48:33 +0530

    Time: 202 ms (80.23%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,8 +1,5 @@
 select
-    case
-        when e.employee_id is null then s.employee_id
-        when s.employee_id is null then e.employee_id
-    end as employee_id
+    coalesce(e.employee_id,s.employee_id) as employee_id
 from employees as e
 full join salaries as s
 on e.employee_id=s.employee_id
