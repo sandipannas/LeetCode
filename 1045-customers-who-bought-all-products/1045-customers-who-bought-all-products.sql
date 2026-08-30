3b06b47aff906c94f8562418042a681b6ed7b52c
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-30 23:57:01 +0530

    Time: 556 ms (34.32%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,7 +1,5 @@
 select  
     c.customer_id
 from customer as c
-inner join product as p
-on c.product_key=p.product_key
 group by c.customer_id
 having count(distinct c.product_key)=(select count(*) from product)
