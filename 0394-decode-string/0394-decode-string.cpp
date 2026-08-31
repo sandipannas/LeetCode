6bc9d795e8da7d5f1b8fd7781ba5c62f20a18b00
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-08-31 23:57:27 +0530

    Time: 0 ms (100%), Space: 9.7 MB (18.94%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -6,7 +6,6 @@ public:
             answer+=word;
         }
         word=answer;
-        //cout<<endl<<"mul_answer-->"<<word;
     }
 
     void operation(vector<string>& stk){
@@ -14,7 +13,7 @@ public:
         
         while(stk.back()!="["){
             string gg=stk.back(); 
-            stk.pop_back();  //cout<<endl<<"pop-->"<<gg;
+            stk.pop_back();
                     
             if(gg[0]>='0' && gg[0]<='9'){
                 int n=stoi(gg);
@@ -26,50 +25,41 @@ public:
         stk.pop_back();
         stk.push_back(opp);
     }
+    
     string decodeString(string s) {
         vector<string> stk; stk.push_back("[");
         string num="";
         string word="";
 
-
         for(char c : s){
-
-            //cout<<endl<<"-- "<<c<<" --"<<endl;
-
             if(c=='['){
                 if(!word.empty()){ 
-                    stk.push_back(word); //cout<<endl<<"pushing-->"<<word;
+                    stk.push_back(word);
                 }
-                stk.push_back(num); //cout<<endl<<"pushing-->"<<num;
-                stk.push_back("["); //cout<<endl<<"pushing-->"<<"[";
+                stk.push_back(num);
+                stk.push_back("[");
                 
                 word=""; num="";
             }
-
             else if(c==']'){
                 if(!word.empty()){
-                    stk.push_back(word); //cout<<endl<<"pushing-->"<<word;
+                    stk.push_back(word);
                     word="";
                 }
-
                 operation(stk); 
-
                 string opp=stk.back(); stk.pop_back();
                 int n=stoi(stk.back()); stk.pop_back();
-
                 mul(n,opp);
-                stk.push_back(opp);  //cout<<endl<<"pushing-->"<<opp;
+                stk.push_back(opp);
             }
             else if(c>='0' && c<='9'){ num+=c; }
             else { word+=c; }
         }
         if(!word.empty()){
-            stk.push_back(word); //cout<<endl<<"pushing-->"<<word;
+            stk.push_back(word);
              word="";
         }
-
         operation(stk);
-
         return stk.back();
     }
 };
