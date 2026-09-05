ef178bf3a63cf5d8bfd64c5414fdb1e71a68caab
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-09-05 05:30:15 +0530

    Time: 3 ms (62.6%), Space: 11.1 MB (90.83%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-       //bullshit edge cases
-       if(k>=num.size()){
-        return "0";
-       }
-       else if(k==0){ return num; }
-       
-        //buid a montonic stack increases only
-        // handle leading zeros 
-        // after passing the num string through monotonic stack if k remains start excluding from the back;
+       if(k>=num.size()) return "0";
+       else if(k==0) return num;
 
-        string answer="";
+       string answer="";
 
-        for(char c:num){
-            //this while loop keeps the string monotonic
+       for(char c:num){
             while(!answer.empty() && answer.back()>c && k!=0){
                     answer.pop_back();
                     k--;
             }
-            
-            //removing leading zeros doesnt affect k
-            if(answer.empty() && c=='0'){ continue; }
+            if(answer.empty() && c=='0') continue; 
 
             answer.push_back(c);
-        }
+       }
 
-        //we still have some leftover k to work with
         while(!answer.empty() && k>0){
             answer.pop_back();
             k--;
         }
 
-        //the string can be empty due to the zeros init 
         return answer==""?"0":answer;
     }
 };
