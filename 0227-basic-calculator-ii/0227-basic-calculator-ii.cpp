5eb1a7206d8a74757acee315b60d0b358d2822ec
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-09-06 21:30:05 +0530

    Time: 19 ms (11.67%), Space: 20.9 MB (5.02%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    // void print(vector<int>& nums){
+    //     for(int i:nums){
+    //         cout<<i<<"->";
+    //     }
+    //     cout<<endl;
+    // }
+    // void print(vector<char>& nums){
+    //     for(char i:nums){
+    //         cout<<i<<"->";
+    //     }
+    //     cout<<endl;
+    // }
+    int calculate(string s) {
+        vector<int> nums;
+        vector<char> sym;
+        
+        //preprocessing
+        string num="";
+        for(char c:s){
+            if(c=='*' || c=='+' || c=='/' || c=='-'){
+                nums.push_back(stoi(num));
+                num="";
+                sym.push_back(c);
+            }
+            else if(c==' '){ continue; }
+            else{
+                num+=c;
+            }
+        }
+        if(!num.empty()) nums.push_back(stoi(num));
+
+        for(int i=0;i<sym.size();i++){
+            if(sym[i]=='-'){
+                sym[i]='+';
+                nums[i+1]*=-1;
+            }
+        }
+        
+        //print(nums); print(sym);
+
+
+        vector<int> tem_nums;
+        vector<char> tem_sym;
+        
+        tem_nums.push_back(nums[0]);
+
+        for(int i=0;i<sym.size();i++){
+            if(sym[i]=='/'){ 
+                tem_nums.back()/=nums[i+1]; 
+            }
+            else if(sym[i]=='*'){ 
+                tem_nums.back()*=nums[i+1]; 
+            }
+            else{
+                tem_nums.push_back(nums[i+1]);
+                tem_sym.push_back(sym[i]);
+            }
+        }
+
+        nums=tem_nums; tem_nums={};
+        sym=tem_sym; tem_nums={};
+
+        tem_nums.push_back(nums[0]);
+        for(int i=0;i<sym.size();i++){
+            tem_nums.back()+=nums[i+1]; 
+        }
+
+        
+        return tem_nums.back();
+    }
+};
