869b88ba481321a7c08a4c7ea6a3832f8c570e43
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-09-06 23:45:20 +0530

    Time: 747 ms (5.17%), Space: 17.1 MB (7.93%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -2,55 +2,60 @@ class Solution {
 public:
     int calculate(string s) {
         vector<int> nums;
-        vector<char> sym;
-        
-        string num="";
-        for(char c:s){
-            if(c=='*' || c=='+' || c=='/' || c=='-'){
-                nums.push_back(stoi(num));
-                num="";
-                sym.push_back(c);
+        vector<char> sys;
+
+        string coll="";
+
+        for(int i=0;i<s.size();i++){
+            char c=s[i];    
+            if( c<'0' || c>'9' || i==s.size()-1 ){ 
+                if(c==' ' && i!=s.size()-1){ continue; }
+                if(i==s.size()-1 && c!=' '){ 
+                    coll+=c;
+                }    
+
+                if(!sys.empty()){
+                    //we have to perform the previous operation
+                    char sign=sys.back();
+
+                    if(sign=='/'){ 
+                        cout<<nums.back()<<'/'<<coll<<endl;
+                        nums.back()/=stoi(coll);  
+                        sys.pop_back(); 
+                        
+                    }
+
+                    else if(sign=='*'){ 
+                        cout<<nums.back()<<'*'<<coll<<endl;
+                        nums.back()*=stoi(coll); 
+                        sys.pop_back();    
+                    }
+
+                    else if(sign=='+'){ 
+                        nums.push_back(stoi(coll)); 
+                    }
+
+                    else if(sign=='-'){ 
+                        nums.push_back(-stoi(coll)); 
+                        sys.back()='+';
+                    }
+                }
+                else{
+                    nums.push_back(stoi(coll)); 
+                }
+                coll=""; 
+                sys.push_back(c);
             }
-            else if(c==' '){ continue; }
             else{
-                num+=c;
-            }
-        }
-        if(!num.empty()) nums.push_back(stoi(num));
-
-        for(int i=0;i<sym.size();i++){
-            if(sym[i]=='-'){
-                sym[i]='+';
-                nums[i+1]*=-1;
-            }
-        }
-        
-        vector<int> tem_nums;
-        vector<char> tem_sym;
-        
-        tem_nums.push_back(nums[0]);
-
-        for(int i=0;i<sym.size();i++){
-            if(sym[i]=='/'){ 
-                tem_nums.back()/=nums[i+1]; 
-            }
-            else if(sym[i]=='*'){ 
-                tem_nums.back()*=nums[i+1]; 
-            }
-            else{
-                tem_nums.push_back(nums[i+1]);
-                tem_sym.push_back(sym[i]);
+                coll+=c;
             }
         }
 
-        nums=tem_nums; tem_nums={};
-        sym=tem_sym; tem_nums={};
-
-        tem_nums.push_back(nums[0]);
-        for(int i=0;i<sym.size();i++){
-            tem_nums.back()+=nums[i+1]; 
+        int answer=0;
+        for(int i:nums){
+            cout<<"adding -> "<<i<<endl;
+            answer+=i;
         }
-
-        return tem_nums.back();
+        return answer;
     }
 };
