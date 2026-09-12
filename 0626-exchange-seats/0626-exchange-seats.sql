58147d811bf04fa375db5a7b29e84e6fd65d5fd7
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-09-12 11:58:15 +0530

    Time: 183 ms (70.92%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,13 +1,9 @@
-with tot as (
-    select count(*) as total from seat
-)            
 select
+    id,
+    coalesce(
     case
-        when tot.total%2=1 and 
-        id=tot.total then id
-        when id%2=0 then id-1
-        else id+1
-    end as id,
-    student
-from seat,tot
+        when id%2=0 then lag(student) over (order by id)
+        else lead(student) over (order by id)
+    end, student) as student
+from seat
 order by id asc
