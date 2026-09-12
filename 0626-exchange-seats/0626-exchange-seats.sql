601eb85667fea4f3fdd3df0123a3de7c240cc2c2
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-09-12 11:54:18 +0530

    Time: 197 ms (39.97%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,13 @@
+with tot as (
+    select count(*) as total from seat
+)            
 select
     case
-        when (select count(*) from seat)%2=1 and 
-        id=(select count(*) from seat) then id
+        when tot.total%2=1 and 
+        id=tot.total then id
         when id%2=0 then id-1
         else id+1
     end as id,
     student
-from seat
+from seat,tot
 order by id asc
