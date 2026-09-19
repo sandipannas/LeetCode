f859c346923757424489190a7647fd758a9f0b3a
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2026-09-19 19:16:50 +0530

    Time: 3 ms (56.02%), Space: 19.3 MB (70.9%) - LeetHub

--- 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.py
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
-        small_odd=float('inf')
-        big_odd=0
 
+        big_odd=0
         small_even=float('inf')
-        big_even=0
 
         map=defaultdict(int)
 
@@ -14,9 +12,7 @@ class Solution:
         for key,value in map.items() :
             if value%2==0 :
                 small_even=min(small_even,value)
-                big_even=max(big_even,value)
             else :
-                small_odd=min(small_odd,value)
                 big_odd=max(big_odd,value)
         
         return big_odd-small_even
